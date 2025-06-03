d51f98a2f721622ed5a7b6d7bf5ad0059f2de685
mchaconl <mchaconl@unemi.edu.ec>
2025-06-03 12:50:30 -0500

    first commit

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -2,9 +2,9 @@
 
 class Conexion{
 	static public function conectar(){
-		$link = new PDO("mysql:host=localhost;dbname=streaming",
-			            "root",
-			            "");
+		$link = new PDO("mysql:host=localhost;dbname=favstream",
+			            "favstream",
+			            "7aLke0OMCZKk7fAcmr00");
 		$link->exec("set names utf8");
 		return $link;
 	}
